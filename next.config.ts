58f64e1b9241b8ff30ba6c06119a783a1168ebb8
Kirill  Dorkin <75579300+kirill-dorkin@users.noreply.github.com>
2025-06-22 01:26:52 +0600

    Add pravatar image host to Next.js config

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
     ],
   },
   async headers() {
