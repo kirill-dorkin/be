b26c9b4caeeaaeef68b65c8789828bf0de48a166
Kirill  Dorkin <75579300+kirill-dorkin@users.noreply.github.com>
2025-06-22 02:50:07 +0600

    Disable Next.js locale detection

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,14 @@
 import createMiddleware from 'next-intl/middleware';
-import { locales, defaultLocale } from './i18n';
+import { locales, defaultLocale, localePrefix } from './i18n';
 
 export default createMiddleware({
   locales: locales as unknown as string[],
   defaultLocale,
   localeDetection: true,
+  localePrefix,
 });
 
 export const config = {
   matcher: ['/((?!api|_next|.*\\..*).*)'],
 };
+
