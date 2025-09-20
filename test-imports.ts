30113a34f25a0c83e158f06a9587622fca755bb8
Kirill  Dorkin <75579300+kirill-dorkin@users.noreply.github.com>
2025-09-20 11:30:05 +0600

    Delete test-imports.ts

--- test-imports.ts
+++
@@ -1,7 +0,0 @@
-// Test file for checking UI component imports
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-
-// This file is used to test the import paths for UI components
-console.log('Button component:', Button)
-console.log('Input component:', Input)
