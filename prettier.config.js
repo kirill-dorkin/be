e8bc2cdb22329f93712951bd2a384bd65e83bc27
Kirill  Dorkin <75579300+kirill-dorkin@users.noreply.github.com>
2025-10-07 00:41:39 +0600

    Delete prettier.config.js

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('prettier').Config} */
-const config = {
-  semi: true,
-  singleQuote: false,
-  trailingComma: "all",
-  plugins: ["prettier-plugin-tailwindcss"],
-};
-
-module.exports = config;
